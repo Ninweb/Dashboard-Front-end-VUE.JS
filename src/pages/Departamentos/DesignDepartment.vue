176dedba9551834c6a233f3c0858b5bbaaa46846
unknown <carlosra210@gmail.com>
2019-02-19 23:01:51 -0430

    fix

---
+++ src/pages/Departamentos/DesignDepartment.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-md-6">
+        <card type="user cartaDepartamento">
+          <p class="card-text">
+          </p>
+          <div class="author">
+            <div class="block block-one colorDiseño"></div>
+            <div class="block block-two colorDiseño"></div>
+            <div class="block block-three colorDiseño"></div>
+            <div class="block block-four colorDiseño"></div>
+            <div class="encima">
+              <i class="fas fa-draw-polygon"></i>
+              <h4 class="title">Diseño Grafico</h4>
+            </div>
+          </div>
+          <p></p>
+        </card>
+      </div>
+
+      <div class="col-md-6">
+        <list-employ-departments/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ListEmployDepartments from './components/ListEmployDepartments.vue'
+export default {
+  components: {ListEmployDepartments}
+}
+</script>
---
+++ src/pages/Departamentos/WebDepartment.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- <card> -->
+    <div class="row">
+      <div class="col-md-6">
+        <card type="user cartaDepartamento">
+          <p class="card-text">
+          </p>
+          <div class="author">
+            <div class="block block-one prueba2"></div>
+            <div class="block block-two prueba2"></div>
+            <div class="block block-three prueba2"></div>
+            <div class="block block-four prueba2"></div>
+            <div class="encima">
+              <i class="fas fa-laptop-code"></i>
+              <h4 class="title">Desarrollo web</h4>
+            </div>
+          </div>
+          <p></p>
+        </card>
+      </div>
+
+      <div class="col-md-6">
+        <list-employ-departments/>
+      </div>
+    </div>
+  <!-- </card> -->
+</template>
+
+<script>
+import ListEmployDepartments from './components/ListEmployDepartments.vue'
+export default {
+  components: {ListEmployDepartments}
+}
+</script>
