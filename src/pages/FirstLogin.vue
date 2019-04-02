7902997325c9a173f76f7346ca7713a8ca4d67f1
unknown <carlosra210@gmail.com>
2019-04-01 22:46:15 -0430

    f

--- src/pages/FirstLogin.vue
+++ src/pages/FirstLogin.vue
@@ -11,128 +11,128 @@
       <tab-content class="tab-content" icon="fas fa-user icon-tab">
         <p id="subtitle-form">Datos personales del empleado</p>
         <label>Nombre</label> <br>
-        <input type="text" v-model="personaEmpleado.nombre" value="this.$store.state.personaData.nombre"/><br>
+        <base-input type="text" v-model="personaEmpleado.nombre"></base-input> <br>
         <label>Apellido</label> <br>
-        <input type="text" v-model="personaEmpleado.apellido" value="this.$store.state.personaData.apellido"/> <br>
+        <base-input type="text" v-model="personaEmpleado.apellido"></base-input> <br>
         <label>Sexo</label> <br>
-        <input type="text" v-model="personaEmpleado.sexo" value="this.$store.state.personaData.sexo"/>
+        <base-input type="text" v-model="personaEmpleado.sexo"></base-input> <br>
         <label>Fecha de nacimiento</label> <br>
-        <input type="date" v-model="personaEmpleado.fecha_nacimiento" value="this.$store.state.personaData.fecha_nacimiento"/>
+        <base-input type="date" v-model="personaEmpleado.fecha_nacimiento"></base-input> <br>
         <label>Cedula de identidad</label> <br>
-        <input type="text" v-model="personaEmpleado.cedula" value="this.$store.state.personaData.cedula"/>
+        <base-input type="text" v-model="personaEmpleado.cedula"></base-input> <br>
         <label>Profesión</label> <br>
-        <input type="text" v-model="personaEmpleado.profesion" value="this.$store.state.personaData.profesion"/>
+        <base-input type="text" v-model="personaEmpleado.profesion"></base-input> <br>
       </tab-content>
 
       <!-- Empleado -->
       <tab-content class="tab-content" icon="fas fa-user-tie icon-tab">
         <p id="subtitle-form">Datos del empleado</p>
         <label>Descripción del cargo</label> <br>
-        <input type="text" v-model="empleado.descripcion_cargo"/>
+        <base-input type="text" v-model="empleado.descripcion_cargo"></base-input> <br>
         <label>Fecha de ingreso</label> <br>
-        <input type="date" v-model="empleado.fecha_ingreso"/>
+        <base-input type="date" v-model="empleado.fecha_ingreso"></base-input> <br>
         <label>Estatus del empleado</label> <br>
-        <input type="text" v-model="empleado.estado_empleado"/>
+        <base-input type="text" v-model="empleado.estado_empleado"></base-input> <br>
         <label>Descripción de la ruta hogar - oficina</label> <br>
-        <input type="text" v-model="empleado.descripcion_transporte_ida"/>
+        <base-input type="text" v-model="empleado.descripcion_transporte_ida"></base-input> <br>
         <label>Descripción de la ruta oficina - hogar</label> <br>
-        <input type="text" v-model="empleado.descripcion_transporte_vuelta"/>
+        <base-input type="text" v-model="empleado.descripcion_transporte_vuelta"></base-input> <br>
         <label>Número de habitación</label> <br>
-        <input type="text" v-model="empleado.numero_habitacion"/>
+        <base-input type="text" v-model="empleado.numero_habitacion"></base-input> <br>
         <label>Número de celular</label> <br>
-        <input type="text" v-model="empleado.numero_celular"/>
+        <base-input type="text" v-model="empleado.numero_celular"></base-input> <br>
         <label>Tipo de sangre</label> <br>
-        <input type="text" v-model="empleado.tipo_sangre"/>
+        <base-input type="text" v-model="empleado.tipo_sangre"></base-input> <br>
         <label>Profesión</label> <br>
-        <input type="text" v-model="empleado.profesion"/>
+        <base-input type="text" v-model="empleado.profesion"></base-input> <br>
         <label>Estado civil</label> <br>
-        <input type="text" v-model="empleado.estado_civil"/>
+        <base-input type="text" v-model="empleado.estado_civil"></base-input> <br>
         <label>Educación</label> <br>
-        <input type="text" v-model="empleado.educacion"/>
+        <base-input type="text" v-model="empleado.educacion"></base-input> <br>
       </tab-content>
 
       <!-- Salario -->
       <tab-content class="tab-content" icon="fas fa-dollar-sign icon-tab">
         <p id="subtitle-form">Sueldo del empleado</p>
         <label>Sueldo base</label> <br>
-        <input type="text" v-model="salario.salario_base"/>
+        <base-input type="text" v-model="salario.salario_base"></base-input> <br>
         <label>Cesta Ticket</label> <br>
-        <input type="date" v-model="salario.salario_ticket"/>
+        <base-input type="date" v-model="salario.salario_ticket"></base-input> <br>
         <label>Seguro</label> <br>
-        <input type="text" v-model="salario.salario_seguro"/>
+        <base-input type="text" v-model="salario.salario_seguro"></base-input> <br>
         <label>Fecha de inicio del sueldo</label> <br>
-        <input type="date" v-model="salario.fecha_inicio"/>
+        <base-input type="date" v-model="salario.fecha_inicio"></base-input> <br>
       </tab-content>
 
       <!-- Direccion -->
       <tab-content class="tab-content" icon="fas fa-map-marked-alt icon-tab">
         <p id="subtitle-form">Dirección del empleado</p>
         <label>Parroquia</label> <br>
-        <input type="text" v-model="direccion.parroquia"/>
+        <base-input type="text" v-model="direccion.parroquia"></base-input> <br>
         <label>Município</label> <br>
-        <input type="date" v-model="direccion.municipio"/>
+        <base-input type="date" v-model="direccion.municipio"></base-input> <br>
         <label>Alcaldía</label> <br>
-        <input type="text" v-model="direccion.alcaldia"/>
+        <base-input type="text" v-model="direccion.alcaldia"></base-input> <br>
         <label>Ciudad</label> <br>
-        <input type="date" v-model="direccion.ciudad"/>
+        <base-input type="date" v-model="direccion.ciudad"></base-input> <br>
         <label>Zona</label> <br>
-        <input type="date" v-model="direccion.zona"/>
+        <base-input type="date" v-model="direccion.zona"></base-input> <br>
       </tab-content>
 
       <!-- Persona - referencia familiar -->
       <tab-content class="tab-content" icon="fas fa-user icon-tab">
         <p id="subtitle-form">Datos de la persona - familiar a referenciar</p>
         <label>Nombre</label> <br>
-        <input type="text" v-model="personaReferenciaFamiliar.nombre"/>
+        <base-input type="text" v-model="personaReferenciaFamiliar.nombre"></base-input> <br>
         <label>Apellido</label> <br>
-        <input type="text" v-model="personaReferenciaFamiliar.apellido"/>
+        <base-input type="text" v-model="personaReferenciaFamiliar.apellido"></base-input> <br>
         <label>Sexo</label> <br>
-        <input type="text" v-model="personaReferenciaFamiliar.sexo"/>
+        <base-input type="text" v-model="personaReferenciaFamiliar.sexo"></base-input> <br>
         <label>Fecha de nacimiento</label> <br>
-        <input type="date" v-model="personaReferenciaFamiliar.fecha_nacimiento"/>
+        <base-input type="date" v-model="personaReferenciaFamiliar.fecha_nacimiento"></base-input> <br>
         <label>Cedula de identidad</label> <br>
-        <input type="text" v-model="personaReferenciaFamiliar.cedula"/>
+        <base-input type="text" v-model="personaReferenciaFamiliar.cedula"></base-input> <br>
         <label>Profesión</label> <br>
-        <input type="text" v-model="personaReferenciaFamiliar.profesion"/>
+        <base-input type="text" v-model="personaReferenciaFamiliar.profesion"></base-input> <br>
       </tab-content>
 
       <!-- Referencia familiar -->
       <tab-content class="tab-content" icon="fas fa-users icon-tab">
         <p id="subtitle-form">Referencia familiar</p>
         <label>Parentezco</label> <br>
-        <input type="text" v-model="referenciaFamiliar.parentezco"/>
+        <base-input type="text" v-model="referenciaFamiliar.parentezco"></base-input> <br>
         <label>Teléfono</label> <br>
-        <input type="date" v-model="referenciaFamiliar.telefono"/>
+        <base-input type="date" v-model="referenciaFamiliar.telefono"></base-input> <br>
         <label>Sexo</label> <br>
-        <input type="text" v-model="referenciaFamiliar.sexo"/>
+        <base-input type="text" v-model="referenciaFamiliar.sexo"></base-input> <br>
       </tab-content>
 
       <!-- Persona - referencia Personal -->
       <tab-content class="tab-content" icon="fas fa-user icon-tab">
         <p id="subtitle-form">Datos de la persona - tercera a referenciar</p>
         <label>Nombre</label> <br>
-        <input type="text" v-model="personaReferenciaPersonal.nombre"/>
+        <base-input type="text" v-model="personaReferenciaPersonal.nombre"></base-input> <br>
         <label>Apellido</label> <br>
-        <input type="text" v-model="personaReferenciaPersonal.apellido"/>
+        <base-input type="text" v-model="personaReferenciaPersonal.apellido"></base-input> <br>
         <label>Sexo</label> <br>
-        <input type="text" v-model="personaReferenciaPersonal.sexo"/>
+        <base-input type="text" v-model="personaReferenciaPersonal.sexo"></base-input> <br>
         <label>Fecha de nacimiento</label> <br>
-        <input type="date" v-model="personaReferenciaPersonal.fecha_nacimiento"/>
+        <base-input type="date" v-model="personaReferenciaPersonal.fecha_nacimiento"></base-input> <br>
         <label>Cedula de identidad</label> <br>
-        <input type="text" v-model="personaReferenciaPersonal.cedula"/>
+        <base-input type="text" v-model="personaReferenciaPersonal.cedula"></base-input> <br>
         <label>Profesión</label> <br>
-        <input type="text" v-model="personaReferenciaPersonal.profesion"/>
+        <base-input type="text" v-model="personaReferenciaPersonal.profesion"></base-input> <br>
       </tab-content>
 
       <!-- Referencia Personal -->
       <tab-content id="tab-content" icon="fas fa-users icon-tab">
         <p id="subtitle-form">Otras referencias</p>
         <label>Parentezco</label> <br>
-        <input type="text" v-model="referenciaPersonal.parentezco"/>
+        <base-input type="text" v-model="referenciaPersonal.parentezco"></base-input> <br>
         <label>Tiempo conocido</label> <br>
-        <input type="date" v-model="referenciaPersonal.tiempo_conocido"/>
+        <base-input type="date" v-model="referenciaPersonal.tiempo_conocido"></base-input> <br>
         <label>Teléfono</label> <br>
-        <input type="text" v-model="referenciaPersonal.telefono"/>
+        <base-input type="text" v-model="referenciaPersonal.telefono"></base-input> <br>
       </tab-content>
     </form-wizard>
   </div>
@@ -146,12 +146,12 @@
       return {
 
         personaEmpleado: {
-          nombre: '', 
-          apellido: '',
-          sexo: '',
-          fecha_nacimiento: '',
-          cedula: '',
-          profesion: '',
+          nombre: this.$store.state.personaData.nombre, 
+          apellido: this.$store.state.personaData.apellido,
+          sexo: this.$store.state.personaData.sexo,
+          fecha_nacimiento: this.$store.state.personaData.fecha_nacimiento,
+          cedula: this.$store.state.personaData.cedula,
+          profesion: this.$store.state.personaData.profesion,
           // correoPersonal: ''
         },
 
@@ -288,16 +288,6 @@
     font-weight: 500;
     font-size: 18px;
     margin: 20px 0;
-  }
-
-  input {
-    width: 100%;
-    padding: 0.5rem 0.7rem;
-    background: transparent;
-    color: white;
-    border-color: #1b64a8;
-    border-radius: 0.4285rem;
-    font-size: 0.75rem;
   }
 
   /* .vue-form-wizard .wizard-nav-pills > li{
